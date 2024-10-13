aff12bbd58126f27ba6c20933c337b02ee10f9b7
Rodrigo Barreto <rodrigofb06@gmail.com>
2024-10-13 20:34:15 -0300

    teste

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,52 +1,61 @@
-
 // Import styling and animation
-import styled from 'styled-components'
-import {motion} from 'framer-motion'
-import {popup} from '../animation'
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { popup } from "../animation";
 //Redux
-import {useDispatch} from 'react-redux'
+import { useDispatch } from "react-redux";
 
-import {loadDetail} from '../actions/detailAction'
+import { loadDetail } from "../actions/detailAction";
 
 // router
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import {smallImage} from '../util'
+import { smallImage } from "../util";
 
-const Game = ({name,released,id,image}) => {
+const Game = ({ name, released, id, image }) => {
+	const stringPathId = id.toString();
+	// Load details
+	const dispatch = useDispatch();
 
-    const stringPathId = id.toString()
-    // Load details
-    const dispatch = useDispatch();
+	const loadDetailHandler = () => {
+		document.body.style.overflow = "hidden";
+		dispatch(loadDetail(id));
+	};
 
-    const loadDetailHandler = () =>{
-        document.body.style.overflow = "hidden"
-        dispatch(loadDetail(id))
-    }
-
-    return(
-        <StyledGame variants={popup} initial="hidden" animate="show" layoutId={stringPathId} onClick={loadDetailHandler}>
-            <Link to={`/game/${id}`}>
-                <motion.h3 layoutId={`title ${stringPathId}`}>{name}</motion.h3>
-                <p>{released}</p>
-                <motion.img layoutId={`image ${stringPathId}`} src={smallImage(image, 640)} alt={name}/>
-            </Link>
-        </StyledGame>
-    )
-}
+	return (
+		<StyledGame
+			variants={popup}
+			initial="hidden"
+			animate="show"
+			layoutId={stringPathId}
+			onClick={loadDetailHandler}
+		>
+			<Link to={`/game/${id}`}>
+				<motion.h3 layoutId={`title ${stringPathId}`}>{name}</motion.h3>
+				{console.log(released)}
+				<p>{released}</p>
+				<motion.img
+					layoutId={`image ${stringPathId}`}
+					src={smallImage(image, 640)}
+					alt={name}
+				/>
+			</Link>
+		</StyledGame>
+	);
+};
 
 const StyledGame = styled(motion.div)`
-    min-height: 30vh;
-    box-shadow: 0px 5px 20px rgba(0,0,0,0.2);
-    text-align: center;
-    border-radius: 1rem;
-    cursor: pointer;
-    overflow: hidden;
-    img{
-        width: 100%;
-        object-fit: cover;
-        height: 40vh;
-    }
-`
+	min-height: 30vh;
+	box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+	text-align: center;
+	border-radius: 1rem;
+	cursor: pointer;
+	overflow: hidden;
+	img {
+		width: 100%;
+		object-fit: cover;
+		height: 40vh;
+	}
+`;
 
-export default Game
+export default Game;
